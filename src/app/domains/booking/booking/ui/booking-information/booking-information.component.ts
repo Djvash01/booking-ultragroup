ad10d3110cc0c5a99c4416de2ec898a5e162933e
David Yepes <djvash01@gmail.com>
2023-08-04 17:47:21 -0500

    feat: 🎸 show details about booking

--- src/app/domains/booking/booking/ui/booking-information/booking-information.component.ts
+++ src/app/domains/booking/booking/ui/booking-information/booking-information.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Booking } from '../../data/models/booking.model';
+import { Hotel } from '@domains/booking/hotels/data/models';
 
 @Component({
   selector: 'app-booking-information',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./booking-information.component.scss']
 })
 export class BookingInformationComponent {
+
+  @Input({
+    required: true
+  }) public booking!: Booking;
+
+  @Input({
+    required: true
+  }) public hotel!: Hotel;
 
 }
--- src/app/domains/booking/dashboard/ui/booking-details/booking-details.component.ts
+++ src/app/domains/booking/dashboard/ui/booking-details/booking-details.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Booking } from '@domains/booking/booking/data/models/booking.model';
+import { BookingService } from '@domains/booking/booking/data/repository/booking.service';
+import { Hotel } from '@domains/booking/hotels/data/models';
+import { HotelService } from '@domains/booking/hotels/data/repository/hotel.service';
 
 @Component({
   selector: 'app-booking-details',
   templateUrl: './booking-details.component.html',
   styleUrls: ['./booking-details.component.scss']
 })
-export class BookingDetailsComponent {
+export class BookingDetailsComponent implements OnInit{
+
+  public hotel!: Hotel;
+  public booking!: Booking;
+
+  constructor(
+    private readonly bookingService: BookingService,
+    private readonly hotelService: HotelService,
+    private readonly route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.getData()
+  }
+
+  private getData(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.getBooking(params.get('bookingId') ?? '');
+    });
+  }
+
+  private getBooking(bookingId: string): void {
+    this.bookingService.getById(bookingId).subscribe((booking) => {
+      this.booking = booking;
+      this.getHotel(booking.hotelId);
+    })
+  }
+
+  private getHotel(hotelId: string): void {
+    this.hotelService.getHotel(hotelId).subscribe((hotel) => {
+      this.hotel = hotel;
+    })
+  }
 
 }
